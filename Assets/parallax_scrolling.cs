a86dd083a3d5d67837940ef3e9147b7a2b743355
AmalH <hichri.amal@hotmail.fr>
2018-06-12 22:36:04 +0100

    initial

---
+++ Assets/parallax_scrolling.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax_scrolling : MonoBehaviour {
+	Renderer rend;
+	public bool paused;
+
+	// Use this for initialization
+	void Start () {
+		rend = this.GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
